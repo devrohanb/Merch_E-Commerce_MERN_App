acf7366baff97f4f80292b8aaddb112e38d7a9b2
devrohanb <dev.rohanb@gmail.com>
2021-04-09 11:18:53 +0530

    After completing backend implementation

--- MERN/projbackend/app.js
+++ MERN/projbackend/app.js
@@ -12,6 +12,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 //DB Connection
 mongoose
@@ -38,6 +39,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
 //PORT
 const port = process.env.PORT || 8000; // process.env.PORT is use to fill the port value provided by remote servers after deployment.
--- MERN/projbackend/routes/order.js
+++ MERN/projbackend/routes/order.js
@@ -5,4 +5,54 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const {} = require("../controllers/order");
+const {
+  getOrderById,
+  createOrder,
+  getAllOrders,
+  getOrderStatus,
+  updateStatus,
+} = require("../controllers/order");
+
+// Params
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+// Actual routes
+// Create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
+
+// read
+router.get(
+  "/order/all/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getAllOrders
+);
+
+// Status of orders
+router.get(
+  "/order/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getOrderStatus
+);
+
+// Update the order
+router.put(
+  "/order/:orderId/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateStatus
+);
+
+module.exports = router;
